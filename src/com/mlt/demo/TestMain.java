845a1d900962593f8abb1d21a1e44bbc57fa946c
cjk <chengjinke@meilintong.com>
2016-10-20 10:13:26 +0800

    Initial commit

---
+++ src/com/mlt/demo/TestMain.java
@@ -0,0 +1,10 @@
+package com.mlt.demo;
+
+/**
+ * Created by CH on 2016/10/20.
+ */
+public class TestMain {
+    public static void main(String args[]){
+        System.out.println("helloworld");
+    }
+}
